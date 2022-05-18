6a4cf8da948b0a767180c43dc02967c83a8052c4
Chiara Gobbi <chiaragobbi@MacBook-Air-di-Chiara.local>
2022-05-18 13:40:00 +0200

    models

---
+++ app/Models/Accomodations.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+class Catalogo{
+
+    const DESCPROD = '<p>Appartamento bello bello</p>';
+
+    protected $_appartamento, $_postoletto;
+
+    public function __construct() {
+        $this->_appartamento = collect(
+                array(
+                    (object) array(
+                        'appId' => '1',
+                        'titolo' => 'Appartamento1',
+                        'desc' => 'Questo è un appartamento',
+                        'data_ins' => '17/05/2022',
+                        'eta_min' => '18',
+                        'eta max' => '25',
+                        'genere' => 'tutti',
+                        'prezzo' => '1000',
+                        'superficie' => '200',
+                        'data_max' => '1/01/2023',
+                        'data_min' => '7/08/2022',
+                        'prov' => 'Ancona',
+                        'citta' => 'Ancona',
+                        'indir' => 'via Crocioni 60',
+                        'CAP' => '60131',
+                        'posti_letto' => '4',
+                        'tipologia' => 'app'
+                    ),
+                )
+        );
+
+        $this->_postoletto = collect(
+                (object) array(
+                        'appId' => '2',
+                        'titolo' => 'PostoLetto2',
+                        'desc' => 'Questo è un posto letto',
+                        'data_ins' => '17/05/2022',
+                        'eta_min' => '18',
+                        'eta max' => '25',
+                        'genere' => 'tutti',
+                        'prezzo' => '1000',
+                        'superficie' => '200',
+                        'data_max' => '1/01/2023',
+                        'data_min' => '7/08/2022',
+                        'prov' => 'Ancona',
+                        'citta' => 'Ancona',
+                        'indir' => 'via Crocioni 60',
+                        'CAP' => '60131',
+                        'posti_letto' => '1',
+                        'tipologia' => 'pl'
+                    )
+        );
+    }
+
+    public function getAppartamento() {
+        return $this->_appartamento;
+    }
+
+    public function getPostoLetto() {
+        return $this->_postoletto;
+    }
+}
