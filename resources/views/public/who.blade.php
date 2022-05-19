432f012e1419e5c6cba683a3a20163f306e4d183
Chiara Gobbi <chiaragobbi@MacBook-Air-di-Chiara.local>
2022-05-19 17:07:18 +0200

    faq

--- resources/views/public/who.blade.php
+++ resources/views/public/who.blade.php
@@ -5,9 +5,11 @@
 @section('content')
 <div class="static">
   <br><center><h1>Chi siamo</h1></center><br>
-  <p>Benvenuti su Kumuuzag!</p>
+  <div class="container text-center">
+  <p class= "text-bold">Benvenuti su Kumuuzag!</p>
   <p>Se sei uno studente questo sito fa al caso tuo. Tutto quello che devi fare è registrarti per scoprire tutte le offerte sugli alloggi disponibili.</p>
   <p>Se sei un insersionista, specificalo nella tua form di sign in per aggiungere nuove insersioni quando vuoi.</p>
   <p>Grazie per aver scelto Kumuuzag!</p>
+</div>
 </div>
 @endsection
