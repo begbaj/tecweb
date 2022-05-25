1140b30c6eb2069f1842dafd8bc69504ff89a298
Chiara <s1093786@studenti.univpm.it>
2022-05-25 17:16:45 +0200

    post rm

--- storage/framework/views/f0afc49266fd47f1c60511338c27ebe0b0165bff.php
+++
@@ -1,8 +0,0 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<?php echo e(URL::asset('css/bootstrap.min.css')); ?>">
-<link rel="icon" type="image/x-icon" href="<?php echo e(URL::asset('favicon.ico')); ?>">
-<script src="https://kit.fontawesome.com/c0c07ca5e1.js" crossorigin="anonymous"></script>
-<script src="<?php echo e(URL::asset('js/bootstrap.bundle.js')); ?>"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<?php /**PATH /opt/lampp/htdocs/tecweb/resources/views/components/_meta.blade.php ENDPATH**/ ?>
