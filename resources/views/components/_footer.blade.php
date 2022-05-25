55c55d17a588f5bed0755be4de43a9ea19460302
Began Bajrami <beganbajrami@gmail.com>
2022-05-25 15:26:42 +0200

    URL::asset fixing references

--- resources/views/components/_footer.blade.php
+++ resources/views/components/_footer.blade.php
@@ -1,7 +1,7 @@
 
 <div class="col">
   <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-      <img src="img/brand/logo.png" height="100">
+      <img src="{{ URL::asset('img/brand/logo.png') }}" height="100">
   </a>
   <p class="text-muted">© 2021</p>
 </div>
@@ -23,7 +23,8 @@
 <div class="col">
   <h5>Social</h5>
   <ul class="nav flex-column">
-    <li><a href="https://www.instagram.com/kumuuzag/" title="Contattaci su instagram">Scrivici su Instagram</a></li>
-    <li><a href="mailto:kumuuzag@gmail.com" title="Mandaci un messaggio">Mandaci una mail</a></li>
+    <li><a href="https://www.instagram.com/kumuuzag/" title="Contattaci su instagram"><i class='fa fa-instagram'></i></a></li>
+    <li><a href="mailto:kumuuzag@gmail.com" title="Mandaci un messaggio"><i class="fa-solid fa-envelope"></i></a></li>
+    <li><a href="https://github.com/begbaj/tecweb/tree/site-laravel"><i class="fa-brands fa-github-square"></i></a></li>
   </ul>
 </div>
