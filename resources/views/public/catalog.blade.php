636080f23b580dca5af5efd020ca5156d78dcdca
Chiara <s1093786@studenti.univpm.it>
2022-05-26 17:26:12 +0200

    admin

--- resources/views/public/catalog.blade.php
+++ resources/views/public/catalog.blade.php
@@ -13,34 +13,34 @@
 </div>
 
 {{Form::open(array('id' => 'filter-form', 'files' => false )) }}
-    <div class='d-flex justify-content-center'>
-        <div class="container d-flex justify-content-center border row border-secondary rounded align-items-center mt-5 pe-5">
+ {{Form::open(array('id' => 'filter-form', 'files' => false )) }}
+    <div class='d-flex justify-content-center align-center'>
+        <div class="container d-flex justify-content-center border row border-secondary rounded align-center mt-5 pe-5 align-items-center">
             <div class ="form-outline row ms-2 mb-4 mt-4 w-25">
                 {{ Form::label('location', 'Località', ['class' => 'col-sm-2 col-form-label', 'for' => 'location']) }}
                 <div class="col-sm-10 ps-3">
-                {{ Form::text('location', '', ['value' => old("location"), 'placeholder'=> 'Località', 'class' => 'form-control ms-4']) }}
+                {{ Form::text('location', '', ['value' => old("location"), 'placeholder'=> 'Località', 'class' => 'form-control ms-5']) }}
                 </div>
             </div>
             
             <div class="form-outline row ms-5 mb-4 mt-4 w-25">
-                {{ Form::label('start-date', 'Inizio', ['class' => 'col-sm-2 col-form-label', 'for' => 'start-date']) }}
+                {{ Form::label('start-date', 'Inizio', ['class' => 'col-sm-3 col-form-label', 'for' => 'start-date']) }}
                 <div class="col-sm-9 ps-3">
-                {{ Form::date('start-date', \Carbon\Carbon::now(), ['value' => old('start-date'), 'class' => 'form-control ms-4']) }}
+                {{ Form::date('start-date', \Carbon\Carbon::now(), ['value' => old('start-date'), 'class' => 'form-control ms-6']) }}
                 </div>
             </div>
 
-            <div class="form-outline row ms-5 mb-4 mt-4 pe-3 w-25">
-                {{ Form::label('end-date', 'Fine', ['class' => 'col-sm-2 col-form-label', 'for' => 'end-date']) }}
+            <div class="form-outline row ms-3 mb-4 mt-4 w-25">
+                {{ Form::label('end-date', 'Fine', ['class' => 'col-sm-3 col-form-label', 'for' => 'end-date']) }}
                 <div class="col-sm-9 ps-3">
-                {{ Form::date('end-date', \Carbon\Carbon::now(), ['value' => old('end-date'), 'class' => 'form-control ms-4']) }}
+                {{ Form::date('end-date', \Carbon\Carbon::now(), ['value' => old('end-date'), 'class' => 'form-control ms-6']) }}
                 </div>
             </div>
-            
             <div class="text-center col pt-2 ps-4">
-                {{ Form::submit('Cerca', ['class' => 'btn btn-primary mb-3 mt-2']) }}
+                {{ Form::submit('Cerca', ['class' => 'btn btn-primary mb-3 mt-2 ms-5']) }}
             </div>
         </div>
-    </div>
+    </div>       
  @endif
 
 @if (auth()->user('admin'))
@@ -62,7 +62,7 @@
             </div>
 
             <div class="form-outline row ms-3 mb-4 mt-4 w-25">
-                {{ Form::label('end-date', 'Fine', ['class' => 'col-sm-2 col-form-label', 'for' => 'end-date']) }}
+                {{ Form::label('end-date', 'Fine', ['class' => 'col-sm-3 col-form-label', 'for' => 'end-date']) }}
                 <div class="col-sm-9 ps-3">
                 {{ Form::date('end-date', \Carbon\Carbon::now(), ['value' => old('end-date'), 'class' => 'form-control ms-6']) }}
                 </div>
