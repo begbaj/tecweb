c93479708cffc0a2a654406b458860d0a2a2f065
AlessioGiacconi <alessio_giacconi@LAPTOP-7D8NBCEH.localdomain>
2022-05-26 15:50:26 +0200

    filtri locatario

--- resources/views/locatario.blade.php
+++ resources/views/locatario.blade.php
@@ -102,13 +102,130 @@
         </div>
     </div>
     
-    <div class="d-flex row border border-secondary rounded justify-content-start mt-3 ms-0" style="width: 35%">
-        <div class="d-flex row justify-content-center">
-            {{ Form::checkbox('name', 'value', null)}}
-            {{ Form::checkbox('cucina', 'Cucina')}}
-            {{ Form::checkbox('cucina', 'Cucina')}}
-        </div> 
-    </div>    
+    <div class="d-flex row border border-secondary rounded mt-3 ms-0" style="width: 35%">
+        <div class="text-center pt-2">
+            <h5>Servizi</h5>
+        </div>
+        
+        <div class ="form-outline d-flex align-items-center me-1 mb-3">
+            <div class="row ms-2">
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('cucina', 'Cucina', null) }}
+                {{ Form::label('cucina', 'Cucina', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'cucina']) }}
+                </div>
+
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('locale-ricreativo', 'Locale Ricreativo', null) }}
+                {{ Form::label('locale-ricreativo', 'Locale Ricreativo', ['class' => 'col-sm-12 col-form-label ps-2', 'for' => 'locale-ricreativo']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('box-doccia', 'Box Doccia', null) }}
+                {{ Form::label('box-doccia', 'Box Doccia', ['class' => 'col-sm-12 col-form-label ps-2', 'for' => 'box-doccia']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('condizionatore', 'Condizionatore', null) }}
+                {{ Form::label('condizionatore', 'Condizionatore', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'condizionatore']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('asciugatrice', 'Asciugatrice', null) }}
+                {{ Form::label('asciugatrice', 'Asciugatrice', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'asciugatrice']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('fumatori', 'Fumatori', null) }}
+                {{ Form::label('fumatori', 'Fumatori', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'fumatori']) }}
+                </div>   
+            </div>
+            
+            <div class="row ps-4">
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('angolo-studio', 'Angolo Studio', null) }}
+                {{ Form::label('angolo-studio', 'Angolo Studio', ['class' => 'col-sm-12 col-form-label ps-2', 'for' => 'angolo-studio']) }}
+                </div>
+
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('vasca', 'Vasca', null) }}
+                {{ Form::label('vasca', 'Vasca', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'vasca']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('wifi', 'WiFi', null) }}
+                {{ Form::label('wifi', 'WiFi', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'wifi']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('lavatrice', 'Lavatrice', null) }}
+                {{ Form::label('lavatrice', 'Lavatrice', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'lavatrice']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('riscaldamento', 'Riscaldamento', null) }}
+                {{ Form::label('riscaldamento', 'Riscaldamento', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'riscaldamento']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('tv', 'TV', null) }}
+                {{ Form::label('tv', 'TV', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'tv']) }}
+                </div>   
+            </div>
+        </div>
+    </div>
+    
+    <div class="d-flex row border border-secondary rounded mt-3 ms-0" style="width: 35%">
+        <div class="text-center pt-2">
+            <h5>Vicino a...</h5>
+        </div>
+        
+        <div class="form-outline d-flex align-items-center me-1 mb-3">
+            <div class="row ms-2">
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('facoltà', 'Facoltà', null) }}
+                {{ Form::label('facoltà', 'Facoltà', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'facoltà']) }}
+                </div>
+
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('supermercato', 'Supermercato', null) }}
+                {{ Form::label('supermercato', 'Supermercato', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'supermercato']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('fermata-metro', 'Fermata Metro', null) }}
+                {{ Form::label('fermata-metro', 'Fermata Metro', ['class' => 'col-sm-12 col-form-label ps-2', 'for' => 'fermata-metro']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('centro-città', 'Centro Città', null) }}
+                {{ Form::label('centro-città', 'Centro Città', ['class' => 'col-sm-12 col-form-label ps-2', 'for' => 'centro-città']) }}
+                </div>
+            </div>
+            
+            <div class="row ms-4">
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('mensa', 'Mensa', null) }}
+                {{ Form::label('mensa', 'Mensa', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'mensa']) }}
+                </div>
+
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('stazione', 'Stazione', null) }}
+                {{ Form::label('stazione', 'Stazione', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'stazione']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('fermata-bus', 'Fermata Bus', null) }}
+                {{ Form::label('fermata-bus', 'Fermata Bus', ['class' => 'col-sm-10 col-form-label ps-2', 'for' => 'fermata-bus']) }}
+                </div>
+                
+                <div class="d-flex align-items-center pt-1">
+                {{ Form::checkbox('palestra', 'Palestra', null) }}
+                {{ Form::label('palestra', 'Palestra', ['class' => 'col-sm-2 col-form-label ps-2', 'for' => 'palestra']) }}
+                </div>
+            </div>
+        </div>
+    </div>
+    {{ Form::close() }}    
 </section>
 @endsection
 
