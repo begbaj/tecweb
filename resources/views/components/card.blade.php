2e8ed4f4ce369c24057d9add0ff1f486f4994576
OT-Rax <rahmi.elmechri@gmail.com>
2022-05-27 16:18:07 +0200

    card footers

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,11 +4,11 @@
     <div class="card-body">
         <h5 class="card-title text-truncate">{{ $accomodation->titolo }}</h5>
         <p class="card-text"> {{ $accomodation->descrizione }} </p>
-        <div class="d-flex justify-content-between align-items-center">
+    </div>
+    <div class="card-footer d-flex justify-content-between align-items-center">
             <div class="btn-group">
                 <a class="btn btn-primary" href="{{ route('login') }}"> Vedi dettagli </a>
             </div>
             <small class="text-muted">{{ $accomodation->created_at }}</small>
-        </div>
     </div>
   </div>
