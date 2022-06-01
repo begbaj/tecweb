9c26cc4dd04548b6d41fc7184561052dad54e982
Began <beganbajrami@gmail.com>
2022-06-01 15:09:54 +0200

    push

--- app/Models/Catalog.php
+++ app/Models/Catalog.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Models\Resources\Alloggio;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Catalog extends Model
 {
-    //
+    public function getAll(){
+        $alloggi = Alloggio::all();
+    }
 }
--- app/Models/Resources/Alloggio.php
+++ app/Models/Resources/Alloggio.php
@@ -6,56 +6,62 @@ use Illuminate\Support\Facades\Log;
 
 class Alloggio extends Model
 {
-	protected $table = 'alloggi';
-	protected $primarykey = 'id';
+    // RESOURCE MODEL DEFINITION
+    protected $table = 'alloggi';
+    protected $primarykey = 'id';
 
-	protected $guarded = ['id'];
+    protected $guarded = ['id'];
 
-	public $timestamps = true;
+    public $timestamps = true;
+    // RESOURCE MODEL DEFINITION
 
-	public function getAlloggiByDate($number=15){
-            $accomodations = Alloggio::orderby('created_at', 'desc')->paginate($number);
-            return $accomodations;
-	}
+    // Custom functions
+    public function getAlloggiByDate($number=15){
+        // in che senso by date?????
+        $accomodations = Alloggio::orderby('created_at', 'desc')->paginate($number);
+        return $accomodations;
+    }
 
-        public function getAlloggioById($id){
-            return Alloggio::all()->where('id', '=', $id);
-        }
-        
-        public function get_alloggio()
-        {
-            $accomodations = Alloggio::all();
-            return $accomodations;
-        }
+    public function getAlloggioById($id){
 
-	public function make_stats($tipo, $data_inizio, $data_fine){
-		$this->_accomodations = new Alloggio;
-                if((is_null($data_inizio)) and is_null($data_fine))
-		{            
-		    $get_filtered = Alloggio::whereRaw('tipo like "%' . $tipo . '%"')->count();
-		}
+        return Alloggio::all()->where('id', '=', $id);
+    }
+    
+    public function get_alloggio()
+    {
+        // nome??
+        $accomodations = Alloggio::all();
+        return $accomodations;
+    }
 
-		else
-		{
-                    $data_inizio = date("Y-m-d",strtotime($data_inizio));
-                    $data_fine = date("Y-m-d",strtotime($data_fine)); 
-		    $get_filtered = Alloggio::whereRaw('tipo like "%'. $tipo .'%" and created_at between "'. $data_inizio. '" and "'.$data_fine .'";')->count();
-		}
-		return $get_filtered;
-	}
-        
-        	public function make_stats3($tipo, $data_inizio, $data_fine){
-		$this->_accomodations = new Alloggio;
-                if((is_null($data_inizio)) and is_null($data_fine))
-		{            
-		    $get_filtered = Alloggio::whereRaw('tipo like "%' . $tipo . '%" and opzionato = 1')->count();
-		}
-		else
-		{
-                    $data_inizio = date("Y-m-d",strtotime($data_inizio));
-                    $data_fine = date("Y-m-d",strtotime($data_fine));
-		    $get_filtered = Alloggio::whereRaw('tipo like "%'. $tipo .'%" and created_at between "'. $data_inizio. '" and "'.$data_fine .'" and opzionato = 1')->count();
-		}
-		return $get_filtered;
-	}
+    public function make_stats($tipo, $data_inizio, $data_fine){
+            $this->_accomodations = new Alloggio;
+            if((is_null($data_inizio)) and is_null($data_fine))
+            {            
+                $get_filtered = Alloggio::whereRaw('tipo like "%' . $tipo . '%"')->count();
+            }
+
+            else
+            {
+                $data_inizio = date("Y-m-d",strtotime($data_inizio));
+                $data_fine = date("Y-m-d",strtotime($data_fine)); 
+                $get_filtered = Alloggio::whereRaw('tipo like "%'. $tipo .'%" and created_at between "'. $data_inizio. '" and "'.$data_fine .'";')->count();
+            }
+            return $get_filtered;
+    }
+    
+            public function make_stats3($tipo, $data_inizio, $data_fine){
+            $this->_accomodations = new Alloggio;
+            if((is_null($data_inizio)) and is_null($data_fine))
+            {            
+                $get_filtered = Alloggio::whereRaw('tipo like "%' . $tipo . '%" and opzionato = 1')->count();
+            }
+            else
+            {
+                $data_inizio = date("Y-m-d",strtotime($data_inizio));
+                $data_fine = date("Y-m-d",strtotime($data_fine));
+                $get_filtered = Alloggio::whereRaw('tipo like "%'. $tipo .'%" and created_at between "'. $data_inizio. '" and "'.$data_fine .'" and opzionato = 1')->count();
+            }
+            return $get_filtered;
+    }
 }
