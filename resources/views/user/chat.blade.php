eb44f252294931692f69895498e29c89ac1ce655
OT-Rax <rahmi.elmechri@gmail.com>
2022-06-06 00:58:38 +0200

    fixed chat

--- resources/views/user/chat.blade.php
+++ resources/views/user/chat.blade.php
@@ -20,6 +20,7 @@ $(function (){
 <div class="page-header py-3 m-8 mx-auto">
 	<h1 class="fw-dark text-center">Chat</h1>
 </div>
+
 @if (count($rubrica)==0)
 	@if (auth()->user()->hasRole('locatore'))
 		<h4 class="text-center">Al momento nessun locatario ha opzionato un tuo alloggio, torna piu' tardi!</h4>
@@ -33,14 +34,15 @@ $(function (){
 		</h4>
 	@endif
 @else
-<div class="d-flex h-75">
-	<div class="deck-columns h-100 overflow-auto" style="max-width:20%">
-	@foreach ($rubrica as $user)
-        	@include ('components.rubricCard', [ '$user' => $user] )
-	@endforeach 
+
+<div class="d-flex overflow-hidden" style="max-height:70%;">
+	<div class="deck-columns overflow-auto" style="width:20%; height:43rem">
+		@foreach ($rubrica as $user)
+			@include ('components.rubricCard', [ '$user' => $user] )
+		@endforeach 
 	</div>
 	<div class="vr"></div>
-	<div class="container h-100" style="max-width:80%">
+	<div class="container" style="width:80%; max-height:70%;">
 		<h5 class="h-20">
 		@if($chatId!=null)
 			{{$rubrica->where('id', $chatId)->first()->nome." ".
@@ -50,18 +52,18 @@ $(function (){
 		@endif
 		</h5>
 		<hr/>
-	<div id="chat-container" class="container overflow-auto" style="height:80%">
-		@foreach ($messaggi as $messaggio)
-			@include ('components.messageCard', [ '$messaggio' => $messaggio])
-		@endforeach 
-	</div>
-	<div>
-	{{ Form::open(array('route' => array(auth()->user()->hasRole('locatario') ? 'chat.send' : 'chat.send', 
-		$chatId ?? $rubrica->first()->id), 'id' => 'sendMessage', 'id_destinatario' => $chatId,'files' => false, 'class'=> 'form-inline d-flex mt-2')) }}
-		{{ Form::text('testo','', ['placeholder'=> 'Messaggio', 'class' => 'form-control m-1']) }}
-		{{ Form::submit('Invia', ['class' => 'btn btn-primary m-1']) }}
-        {{ Form::close() }}
-	</div>
+		<div id="chat-container" class="container overflow-auto" style="height:37rem">
+			@foreach ($messaggi as $messaggio)
+				@include ('components.messageCard', [ '$messaggio' => $messaggio])
+			@endforeach 
+		</div>
+		<div>
+			{{ Form::open(array('route' => array(auth()->user()->hasRole('locatario') ? 'chat.send' : 'chat.send', 
+				$chatId ?? $rubrica->first()->id), 'id' => 'sendMessage', 'id_destinatario' => $chatId,'files' => false, 'class'=> 'form-inline d-flex mt-2')) }}
+				{{ Form::text('testo','', ['placeholder'=> 'Messaggio', 'class' => 'form-control m-1']) }}
+				{{ Form::submit('Invia', ['class' => 'btn btn-primary m-1']) }}
+			{{ Form::close() }}
+		</div>
 	</div>
 </div>
 @endif
