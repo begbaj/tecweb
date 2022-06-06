1dcf6437315113c2d582b7c65bcd0c84d54c0ce4
chiara <s1093786@studenti.univpm.it>
2022-06-06 17:45:44 +0200

    stats

--- app/Models/Rented.php
+++ app/Models/Rented.php
@@ -20,14 +20,15 @@ class Rented
             $messages = new Messaggio;
             if((is_null($data_inizio)) and is_null($data_fine))
             {  
-                $_rented = Alloggio::join('messaggi', 'messaggi.id_alloggio','=','alloggi.id')->count();
+                $_rented = Alloggio::join('messaggi', 'messaggi.id_alloggio','=','alloggi.id')->whereRaw(' tipo like "%'. $tipo .'" and messaggi.id_alloggio IS NOT NULL')->count();
             }    
             else
             {
                 $data_inizio = date("Y-m-d",strtotime($data_inizio));
                 $data_fine = date("Y-m-d",strtotime($data_fine));
-                $_rented = Alloggio::join('messaggi', 'messaggi.id_alloggio','=','alloggi.id')->whereRaw(' tipo like "%'. $tipo .'%" and alloggi.created_at between "'. $data_inizio. '" and "'.$data_fine .'" and opzionato = 1')->count();
+                $_rented = Alloggio::join('messaggi', 'messaggi.id_alloggio','=','alloggi.id')->whereRaw(' tipo like "%'. $tipo .'%" and alloggi.created_at between "'. $data_inizio. '" and "'.$data_fine .'" and messaggi.id_alloggio IS NOT NULL')->count();
             }
+            Log::debug('query = '. $_rented);
             return $_rented;
 	}
 }
