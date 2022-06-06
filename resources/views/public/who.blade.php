7a215bbbe41cb65b8e23d243c6f063f3c8a74106
chiara <s1093786@studenti.univpm.it>
2022-06-06 19:10:52 +0200

    inserzionista con la zzzz

--- resources/views/public/who.blade.php
+++ resources/views/public/who.blade.php
@@ -8,7 +8,7 @@
   <div class="container text-center">
   <p class= "text-bold">Benvenuti su Kumuuzag!</p>
   <p>Se sei uno studente questo sito fa al caso tuo. Tutto quello che devi fare è registrarti per scoprire tutte le offerte sugli alloggi disponibili.</p>
-  <p>Se sei un insersionista, specificalo nella tua form di sign in per aggiungere nuove insersioni quando vuoi.</p>
+  <p>Se sei un inserzionista, specificalo nella tua form di sign in per aggiungere nuove insersioni quando vuoi.</p>
   <p>Grazie per aver scelto Kumuuzag!</p>
 </div>
 </div>
