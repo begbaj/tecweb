3df19191227acd8e09641e11d02a0686791b6a7c
Began Bajrami <beganbajrami@gmail.com>
2022-06-07 14:56:53 +0200

    long cards

--- resources/views/components/longcard.blade.php
+++ resources/views/components/longcard.blade.php
@@ -1,39 +1,42 @@
 {{-- Long Card: long card used to show a list of accomodations--}}
-<div class="card mb-3" style="max-width: 50em;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img class="img-fluid rounded-start card-img" src="{{ asset('assets/'. $accomodation->id . '/thumbnail')}} " alt="" style="height: 300px">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title"> {{ $accomodation->titolo }} </h5>
-        <p class="card-text"> {{ substr($accomodation->descrizione, 0, 150) }} </p>
-        <p class="card-text"> Creato il: {{ $accomodation->created_at}} </p>
-        <p class="card-text">
-          <small class='text-muted'> @if ($accomodation->updated_at != null) Ultimo aggiornamento: {{ $accomodation->updated_at}} @else Alloggio mai aggiornato @endif </small>
-        </p>
-        <div class='row'>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <a class="btn btn-secondary col mx-2" href="{{ Route('lore.accom.edit', [$accomodation->id])}}">Modifica</a>
-            <button type="button" class="btn btn-danger col mx-2" data-bs-toggle="modal" data-bs-target="#delete-{{$accomodation->id}}"">Elimina</button>
-							<div class=" modal fade" id="delete-{{$accomodation->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Elimina Alloggio {{$accomodation->titolo}}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body"> Confermi di voler procedere con l'eliminazione? </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                    <a type="button" href='{{ route("lore.accom.delete", ["id" => $accomodation->id]) }}' class="btn btn-primary">Confermo</a>
-                  </div>
-                </div>
-              </div>
+<div class="card m-5">
+    <div class="row g-0">
+       <div class="col-md-4 overflow-hidden" style="height: 15em;" >
+          <img class="card-img" src="{{ asset('assets/'. $accomodation->id . '/thumbnail')}}">
+       </div>
+       <div class="col-md-8">
+          <div class="card-body">
+             <h5 class="card-title"> {{ $accomodation->titolo }} </h5>
+             <p class="card-text"> {{ substr($accomodation->descrizione, 0, 150) }} </p>
+             <p class="card-text"> Creato il: {{ $accomodation->created_at}} </p>
+             <p class="card-text">
+                <small class='text-muted'> @if ($accomodation->updated_at != null) Ultimo aggiornamento: {{ $accomodation->updated_at}} @else Alloggio mai aggiornato @endif </small>
+             </p>
           </div>
-          <a class="btn btn-primary col mx-2" href="{{ route('catalog.accom.details', [$accomodation->id]) }}">Visualizza</a>
-        </div>
-      </div>
+       </div>
     </div>
-  </div>
+    <div class="row g-0 bg-light rounded border py-3 px-2 justify-content-start"> 
+        <div class="col-4">
+            <a class="d-flex btn btn-primary me-2" href="{{ route('catalog.accom.details', [$accomodation->id]) }}">Visualizza</a>
+        </div>
+        <div class="col-5">
+            <a class=" btn btn-secondary mx-2" href="{{ Route('lore.accom.edit', [$accomodation->id])}}">Modifica</a>
+            <button type="button" class="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#delete-{{$accomodation->id}}">Elimina</button>
+            <div class=" modal fade" id="delete-{{$accomodation->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+               <div class="modal-dialog">
+                  <div class="modal-content">
+                     <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Elimina Alloggio {{$accomodation->titolo}}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                     </div>
+                     <div class="modal-body"> Confermi di voler procedere con l'eliminazione? </div>
+                     <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                        <a type="button" href='{{ route("lore.accom.delete", ["id" => $accomodation->id]) }}' class="btn btn-primary">Confermo</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+        </div>
+    </div>
 </div>
